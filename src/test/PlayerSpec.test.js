2c5369ab8c5dd2afd44f4c2810f325b45aaaa9b3
Sindhu Pothireddy <sindhu.pothireddy@gmail.com>
2014-06-24 15:32:42 -0700

    failed test

--- src/test/PlayerSpec.test.js
+++ src/test/PlayerSpec.test.js
@@ -17,7 +17,25 @@ describe("Player", function() {
     expect(player).toBePlaying(song);
   });
 
-  
+  describe("when song has been paused", function() {
+    beforeEach(function() {
+      player.play(song);
+      player.pause();
+    });
+
+    it("should indicate that the song is currently paused", function() {
+      expect(player.isPlaying).toBeFalsy();
+
+      // demonstrates use of 'not' with a custom matcher
+      expect(player).not.toBePlaying(song);
+    });
+
+    it("should be possible to resume", function() {
+      player.resume();
+      expect(player.isPlaying).toBeTruthy();
+      expect(player.currentlyPlayingSong).toEqual(song);
+    });
+  });
 
   // demonstrates use of spies to intercept and test method calls
   it("tells the current song if the user has made it a favorite", function() {
@@ -26,7 +44,7 @@ describe("Player", function() {
     player.play(song);
     player.makeFavorite();
 
-    expect(song.persistFavoriteStatus).toHaveBeenCalledWith(true);
+    expect(song.persistFavoriteStatus).toHaveBeenCalledWith(false);
   });
 
   //demonstrates use of expected exceptions
