cb9c7fd0675cfb13ef7de951b824a22f9bafe135
Sindhu Pothireddy <sindhu.pothireddy@gmail.com>
2014-06-24 15:28:25 -0700

    deleted 2 tests

--- src/js/Player.js
+++ src/js/Player.js
@@ -4,7 +4,6 @@ Player.prototype.play = function(song) {
   this.currentlyPlayingSong = song;
   this.isPlaying = true;
 };
-
 Player.prototype.pause = function() {
   this.isPlaying = false;
 };
--- src/test/PlayerSpec.test.js
+++ src/test/PlayerSpec.test.js
@@ -17,25 +17,7 @@ describe("Player", function() {
     expect(player).toBePlaying(song);
   });
 
-  describe("when song has been paused", function() {
-    beforeEach(function() {
-      player.play(song);
-      player.pause();
-    });
-
-    it("should indicate that the song is currently paused", function() {
-      expect(player.isPlaying).toBeFalsy();
-
-      // demonstrates use of 'not' with a custom matcher
-      expect(player).not.toBePlaying(song);
-    });
-
-    it("should be possible to resume", function() {
-      player.resume();
-      expect(player.isPlaying).toBeTruthy();
-      expect(player.currentlyPlayingSong).toEqual(song);
-    });
-  });
+  
 
   // demonstrates use of spies to intercept and test method calls
   it("tells the current song if the user has made it a favorite", function() {
